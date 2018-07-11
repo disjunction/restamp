620d0bf602f44e470e9efb96af5112ccca3731ba
Konstantin Pogorelov <or@pluseq.com>
2018-07-11 02:36:43 -0400

    add PHP examples

---
+++ test/data/sample/php/mixed-html-no-license.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+  hello
+
+<? echo "world!"; ?>
+
+</body>
+</html>
---
+++ test/data/sample/php/sample-outdated.php
@@ -0,0 +1,10 @@
+<?php
+/*
+ * Copyright (c) some OLD company
+ * This is a license
+ */
+php?>Some contents
+
+<?php
+$a = 123;
+echo $a;
